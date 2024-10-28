dfdfabd05b56c712efa862a79c48c327f09accd0
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-10-29 09:58:11 +1100

    confusion on function

---
+++ script.js
@@ -0,0 +1,21 @@
+//initialize it
+//call it 
+//invoke it 
+
+//Life Cycle of function 
+// debugger
+
+
+
+const a = hello;
+console.log(a);
+
+const hello =()=>{
+    console.log("Hello");
+    
+}
+
+// function hello (){
+//     console.log("Hello");
+    
+// }
